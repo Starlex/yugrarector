763fce05bb41c12832e7ef547c63ba4aadd482b2
Starostenko Aleksei <staralforwork@gmail.com>
2014-02-05 17:18:17 +0600

    Some updates

--- admin/block/db.php
+++ admin/block/db.php
@@ -1,7 +1,7 @@
 <?php
 
-$link = mysql_connect ('mysql.ngguadm.z8.ru', 'dbu_ngguadm_6', 'eE9XwiUkCUW') or die('connection fail');
+$link = mysql_connect ('localhost', '****', '*****') or die('connection fail');
 mysql_set_charset('utf8', $link);
-mysql_select_db ('db_ngguadm_9') or die ('connection fail');
+mysql_select_db ('****') or die ('connection fail');
 
 ?>
